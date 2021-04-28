96f19e396b26cff7aedd83441e8e2c2fc9e253f0
William McCumsite <william.mccumstie@gmail.com>
2021-04-28 13:27:48 +0100

    Replace verify loop with a timer task

--- config/initializers/desktop_types.rb
+++ config/initializers/desktop_types.rb
@@ -31,26 +31,19 @@
 return if ENV['RACK_ENV'] == 'test'
 
 # Periodically reload and verify the desktops
-Thread.new do
-  count = 0
-  loop do
-    models = SystemCommand.avail_desktops(user: ENV['USER'])
-                          .tap(&:raise_unless_successful)
-                          .stdout
-                          .each_line.map do |line|
-      data = line.split("\t")
-      home = data[2].empty? ? nil : data[2]
-      Desktop.new(name: data[0], summary: data[1], homepage: home)
-    end
-
-    models.each { |m| m.verify_desktop(user: ENV['USER']) }
-    hash = models.map { |m| [m.name, m] }.to_h
-
-    Desktop.instance_variable_set(:@cache, hash)
-    DEFAULT_LOGGER.info "Finished #{'re' if count > 0 }loading the desktops"
-    count += 1
-
-    sleep FlightDesktopRestAPI.config.refresh_rate
+Concurrent::TimerTask.new(execution_interval: FlightDesktopRestAPI.config.refresh_rate, run_now: true) do
+  models = SystemCommand.avail_desktops(user: ENV['USER'])
+                        .tap(&:raise_unless_successful)
+                        .stdout
+                        .each_line.map do |line|
+    data = line.split("\t")
+    home = data[2].empty? ? nil : data[2]
+    Desktop.new(name: data[0], summary: data[1], homepage: home)
   end
-end
 
+  models.each { |m| m.verify_desktop(user: ENV['USER']) }
+  hash = models.map { |m| [m.name, m] }.to_h
+
+  Desktop.instance_variable_set(:@cache, hash)
+  DEFAULT_LOGGER.info "Finished (re)loading the desktops"
+end.execute
